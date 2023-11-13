9f8f2cb90389ce6ddd9f6fdf0760923401c298ca
TitaRuiz <ggp2002es@gmail.com>
2023-11-13 18:11:48 +0100

    Initial commit

---
+++ src/com/corenetworks/modelo/AccesoDesarrollo.java
@@ -0,0 +1,8 @@
+package com.corenetworks.modelo;
+
+public class AccesoProduccion implements IDAO{
+    @Override
+    public String insertar(Cliente c1) {
+        return "Se inserta el cliente en PRODUCCION " + c1.getNombre();
+    }
+}
